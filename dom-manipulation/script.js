da01612fd4d717a3762f7e4bc8d472d874782d17
Alcapone00 <horis229@gmail.com>
2024-05-04 17:47:56 +0200

    main

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,23 +1,39 @@
-var username = document.forms['form']['username']
-var password = document.forms['form']['password']
+var username = document.forms['form']['username'];
+var password = document.forms['form']['password'];
 
-var email_error = document.getElementsById('wrongEmail')
-var pass_error = document.getElementsById('wrongPassword')
+var email_error = document.getElementById('wrongEmail');
+var pass_error = document.getElementById('wrongPassword');
+
+username.addEventListener('textInput', emailVerify);
+password.addEventListener('textInput', passVerify);
 
 function validated(){
     if(username.value.length < 8){
+        username.focus();
         username.style.border = "1px solid red";
         email_error.style.display = "block";
-        username.focus();
+        return false
+    }
+    if(password.value.length < 6){
+        password.focus();
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
         return false
     }
 }
 
-function validated(){
-    if(password.value.length < 8){
-        password.style.border = "1px solid red";
-        pass_error.style.display = "block";
-        password.focus();
-        return false
+function emailVerify(){
+    if(username.value.length <= 8){
+        username.style.border = "1px solid"
+        email_error.style.display = "none"
+        return true
+    }
+}
+
+function passVerify(){
+    if(password.value.length <= 6){
+        password.style.border = "1px solid"
+        pass_error.style.display = "none"
+        return true
     }
 }
