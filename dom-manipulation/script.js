30c0c58c658f812ef12516a9bd6ab9fe1029f7bf
Alcapone00 <horis229@gmail.com>
2024-05-05 21:05:34 +0200

    main

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,14 +1,22 @@
 var username = document.forms['form']['username'];
+var accountUsername = document.forms['form']['accountUsername'];
 var password = document.forms['form']['password'];
+var secondPassword = document.forms['form']['secondPassword'];
 var usernameRecovery = document.forms['form']['usernameRecovery'];
 
+var username_error = document.getElementById('wrongUsername');
 var email_error = document.getElementById('wrongEmail');
 var pass_error = document.getElementById('wrongPassword');
+var secondPass_error = document.getElementById('wrongSecondPassword');
 var recovery = document.getElementById('recovery');
+var buttonRecovery = document.getElementById('restoreButton');
 
+accountUsername.addEventListener('textInput', usernameVerify);
 username.addEventListener('textInput', emailVerify);
 password.addEventListener('textInput', passVerify);
 usernameRecovery.addEventListener('textInput', recoveryRemove);
+
+// email recovery
 
 function validated(){
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -46,6 +54,8 @@ function passVerify(){
 }
 
 
+// Account recovery
+
 function recoveryValidated(){
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -60,11 +70,63 @@ function recoveryValidated(){
 function recoveryRemove(){
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(!emailRegex.test(usernameRecovery.value)){
-        usernameRecovery.focus();
+    if(emailRegex.test(usernameRecovery.value)){
         usernameRecovery.style.display = "none";
-        recoveryMessage.style.display = "block";
+        buttonRecovery.style.display = "none";
+        email_error.style.display = "none";
+        recovery.style.display = "block";
+        recovery.style.marginTop = "20px";
+        recovery.style.marginBottom = "60px";
         return true
     }
-
 }
+
+// create account 
+
+function usernameValidated(){
+    if(accountUsername.value.length < 6){
+        accountUsername.focus();
+        accountUsername.style.border = "1px solid red";
+        username_error.style.display = "block";
+        return false
+    }
+    if(!completeValidated()){
+        return false
+    }
+}
+
+function completeValidated(){
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(username.value)){
+        username.focus();
+        username.style.border = "1px solid red";
+        email_error.style.display = "block";
+        return false
+    }
+    if(password.value.length < 6){
+        password.focus();
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        return false
+    }
+    if(secondPassword.value != password.value){
+        secondPassword.focus();
+        secondPassword.style.border = "1px solid red";
+        secondPass_error.style.display = "block";
+        return false
+    }
+    return true
+}
+
+
+function usernameVerify(){
+    if(accountUsername.value.length <= 6){
+        accountUsername.style.border = "1px solid"
+        username_error.style.display = "none"
+        return true
+    }
+}
+
+
+
